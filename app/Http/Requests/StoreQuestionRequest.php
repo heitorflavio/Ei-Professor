6885ce7901cfa82980e36a84cc9ec4b305eb29c0
heitorflavio <heitor.ribeiro@polvo.com.br>
2024-08-14 10:25:03 -0300

    feat: Add validation rule for minimum question length
    This commit adds a validation rule to the `StoreQuestionRequest` class to enforce a minimum length of 10 characters for the `question` field. This ensures that questions submitted by users are of sufficient length.

--- app/Http/Requests/StoreQuestionRequest.php
+++ app/Http/Requests/StoreQuestionRequest.php
@@ -23,7 +23,7 @@ class StoreQuestionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'question' => ['required', 'string'],
+            'question' => ['required', 'string', 'min:10'],
         ];
     }
 }
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -19,3 +19,18 @@ it('should be able to create a new question bigger than 255 characters', functio
         'question' => str_repeat('*', 260).'?',
     ]);
 });
+
+it('should have at least 10 characters', function () {
+    // Arrange :: preparar
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    // Act :: agir
+    $request = $this->post(route('question.store'), [
+        'question' => str_repeat('*', 8).'?',
+    ]);
+
+    // Assert :: verificar
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['attribute' => 'question', 'min' => 10])]);
+    \Pest\Laravel\assertDatabaseCount('questions', 0);
+});
