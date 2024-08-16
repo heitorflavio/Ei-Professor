d900ce7be7c4fa9718f299ca71cfbf3ee8cbba6c
heitorflavio <heitor.ribeiro@polvo.com.br>
2024-08-15 23:45:11 -0300

    feat: Update VoteController to use User model for liking a question

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class VoteController extends Controller
@@ -12,7 +13,9 @@ class VoteController extends Controller
      */
     public function __invoke(Request $request, Question $question): \Illuminate\Http\RedirectResponse
     {
-        auth()->user()->like($question);
+        /** @var User $user */
+       $user = auth()->user();
+       $user->like($question);
 
         return redirect()->route('dashboard');
     }
